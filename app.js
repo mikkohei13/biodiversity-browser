456cc24a8e7233eea3aed7cef393bb26b1289fe3
Mikko Heikkinen <hawk@biomi.org>
2016-12-26 20:48:18 +0200

    Refactoring, commenting, cleaning

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ console.log("Elastic");
 console.log("=====================");
 
 /*
+TODO:
 Check if return exact matches or partial matches
+Search button
 */
 
+// -----------------------------------
+// EVENTS
+
+// Page load
 $(document).ready(function() {
 	$("#query").text("Total");
 	getAll();
 	let debugName = "Luscinia luscinia"; dataQuery(debugName); $("#query").text("Debugging with " + debugName); // DEBUG
 });
 
+// Name search (enter)
 $("#species").keypress(function(event) {
 	if (event.which == 13) {
 		let species = $("#species").val();
@@ -29,6 +36,27 @@ $("#species").keypress(function(event) {
 //		console.log(species);
 	}
 });
+
+// -----------------------------------
+// QUERY ELASTIC
+
+// Get summary of all data
+function getAll() {
+	$.ajax({
+		method: "GET",
+		url: "http://192.168.56.10:9200/baltic-aves/_search",
+		beforeSend: function (xhr) {
+		    xhr.setRequestHeader ("Authorization", "Basic " + btoa("elastic" + ":" + "changeme"));
+		}
+	})
+	.done(function(elasticData) {
+//		console.log( data );
+		let count = elasticData.hits.total;
+		let countFormatted = count.toLocaleString();
+		$("#total").text(countFormatted);
+		$("#container").html("");
+	});
+}
 
 function dataQuery(species) {
 	let queryData = JSON.stringify({
@@ -56,50 +84,27 @@ function dataQuery(species) {
 		    xhr.setRequestHeader ("Authorization", "Basic " + btoa("elastic" + ":" + "changeme"));
 		}
 	})
-	.done(function(dataobject) {
-		console.log(dataobject);
+	.done(function(elasticData) {
+		console.log(elasticData);
 
-		let observationsPerMonth = getObservationsPerMonth(dataobject);
-
-		// Basic chart
-//		printMontlhyChart(observationsPerMonth);
+		let observationsPerMonth = getObservationsPerMonth(elasticData);
 
 		// Highcharts
 		printHighchart(observationsPerMonth, species);
 
 		// Show count
-		let count = dataobject.hits.total;
+		let count = elasticData.hits.total;
 		let countFormatted = count.toLocaleString();
 		$("#total").text(countFormatted);
 	});
 }
 
-function printMontlhyChart(observationsPerMonth)
+// -----------------------------------
+// FORMAT DATA
+
+function getObservationsPerMonth(elasticData)
 {
-	console.log("-----");
-	let html = "<!-- Monthly chart: -->\n";
-
-	for (let m = 1; m <= 12; m++) {
-		let count = observationsPerMonth[m];
-		let width;
-
-		// Handle missing months
-/*		if (undefined == count)
-		{
-			count = 0;
-		}
-		*/
-		width = count / 100;
-
-		html = html + "<span style='width: " + width + "px;' class='bar month" + m + "'>&nbsp;</span>" + count + "\n";
-	}
-	console.log(html);
-	$("#chart").html(html);
-}
-
-function getObservationsPerMonth(dataobject)
-{
-	let monthlyBuckets = dataobject.aggregations.observationsPerMonth.buckets;
+	let monthlyBuckets = elasticData.aggregations.observationsPerMonth.buckets;
 	let monthlyObservations = {};
 
 	console.log(monthlyBuckets);
@@ -132,38 +137,6 @@ function fillMissingMonths(monthlyObservations)
 	return filledMonthlyObservations;
 }
 
-function getSpecies(species) {
-	$.ajax({
-		method: "GET",
-		url: "http://192.168.56.10:9200/baltic-aves/_search?q=species:" + species,
-		beforeSend: function (xhr) {
-		    xhr.setRequestHeader ("Authorization", "Basic " + btoa("elastic" + ":" + "changeme"));
-		}
-	})
-	.done(function(data) {
-//		console.log( data );
-		let count = data.hits.total;
-		let countFormatted = count.toLocaleString();
-		$("#total").text(countFormatted);
-	});
-}
-
-function getAll(species) {
-	$.ajax({
-		method: "GET",
-		url: "http://192.168.56.10:9200/baltic-aves/_search",
-		beforeSend: function (xhr) {
-		    xhr.setRequestHeader ("Authorization", "Basic " + btoa("elastic" + ":" + "changeme"));
-		}
-	})
-	.done(function(data) {
-//		console.log( data );
-		let count = data.hits.total;
-		let countFormatted = count.toLocaleString();
-		$("#total").text(countFormatted);
-	});
-}
-
 // Highcharts
 function printHighchart(observationsPerMonth, species)
 {
@@ -172,13 +145,14 @@ function printHighchart(observationsPerMonth, species)
 	$(function () { 
 	    var myChart = Highcharts.chart('container', {
 	        chart: {
-	            type: 'bar'
+	            type: 'column'
 	        },
 	        title: {
 	            text: species
 	        },
 	        xAxis: {
-	            categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+//	            categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+	            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
 	            title: {
 	            	text: 'Month'
 	            }
@@ -188,7 +162,14 @@ function printHighchart(observationsPerMonth, species)
 	                text: 'Observations'
 	            }
 	        },
-	        series: data
+	        series: data,
+	        plotOptions: {
+	        	series: {
+	        		animation: {
+	        			duration: 10
+	        		}
+	        	}
+	        }
 	    });
 	});
 }
